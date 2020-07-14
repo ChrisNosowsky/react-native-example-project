94a222e72f132476671865f405f2433b4fdc3b65
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-07-13 22:40:26 -0400

    Styling, testing react components, dealing with nested view text components that won't inherit styling

--- App.js
+++ App.js
@@ -5,8 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem <Text>ipsum</Text> dolor sit amet.</Text>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet.</Text>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet.</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +24,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: "pink",
+    padding: 20
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    fontWeight: 'bold'
+  }
 });
